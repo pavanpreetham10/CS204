29610605e0e05128d0b951b3e6bb2f895f8d9bde
pavanpreetham10 <53636009+pavanpreetham10@users.noreply.github.com>
2019-09-02 17:16:48 +0530

    Update lab6.cpp

--- lab6.cpp
+++ lab6.cpp
@@ -1,41 +1,61 @@
 #include<bits/stdc++.h>
-#include<vector>
 using namespace std;
+int search(vector <int> v,int s)
+{
+	int i;
+	int size=v.size();
+	if(size==0) { return 0;}
+	for(i=0;i<size;i++)
+	{
+		if(v[i]==s) { return i+1; }
+	}
+return 0;
+}
 int main()
 {
 int n,l;
-vector <int> v;
-cin >> n;
-cin >> l;
-int i,j;
-int a,u,r;
-for(i=0;i<n;i++)
+int o,u,r;
+int i;
+vector <int> v,V;
+cin >> n >> l;
+for(i=0;i<l;i++)
 {
-	v.push_back(0);
-}
-for(j=0;j<l;j++)
-{
-	cin >> a;
-	if(a==1)
+	cin >> o;
+	if(o==1) 
 	{
 		cin >> u >> r;
-		v[u-1]=v[u-1]+r;
-	}
-	else if(a==2)
-	{
-		int max,k,p;
-		p=1;
-		max=v[0];
-		if(n>1)
-		{	
-			for(k=0;k<(n-1);k++)	
-			{
-				if(max<v[k+1]) { max=v[k+1]; p=k+2; }
-			}
-			if(max==0) {cout << "No data available." << "\n";}
-			else { cout << p << "\n"; }
+		int s=search(v,u);
+		if(s==0) 
+		{
+			v.push_back(u);
+			V.push_back(r);
+		}
+		else
+		{
+			V[s-1]=V[s-1]+r;
+			//cout << V[s-1];
 		}
 	}
+	else if(o==2)
+	{
+		int size=v.size();
+		int highest,p,k;
+		p=1;
+		highest=0;
+		if(size)
+		{
+			highest=V[0];
+		}
+		if(size>1)
+		{
+			for(k=0;k<size;k++)
+			{
+				if(highest<V[k+1]) { highest=V[k+1]; p=v[k+1]; }
+			}
+		}
+		if(highest==0) {cout << "no data" << "\n";}
+		else { cout << p << "\n"; }
+	}
 }
+return 0;
 }
-
